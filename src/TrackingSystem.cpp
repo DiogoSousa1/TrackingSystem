32dba4fc4c5fd48e68096e252dfc6ac6652c3dd3
DiogoSousa1 <dfb.sousa@campus.fct.unl.pt>
2021-08-06 16:33:20 +0100

    initial commit

---
+++ src/TrackingSystem.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TrackingSystem.cpp
+// Author      : Barata
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <librealsense2/rs.hpp>
+#include <apriltag/apriltag.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
