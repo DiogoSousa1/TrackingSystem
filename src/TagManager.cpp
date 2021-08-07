b56e5b6e9f49893f384266acb56e081060dbede6
DiogoSousa1 <dfb.sousa@campus.fct.unl.pt>
2021-08-07 15:45:14 +0100

    Initial commit in vs

--- src/TagManager.cpp
+++ src/TagManager.cpp
@@ -4,7 +4,7 @@
  *  Created on: 06/08/2021
  *      Author: barata
  */
-
+#include <iostream>
 #include "TagManager.h"
 
 Tag_Manager::Tag_Manager() {
--- src/TrackingSystem.cpp
+++ src/TrackingSystem.cpp
@@ -10,12 +10,15 @@
 #include <librealsense2/rs.hpp>
 #include <apriltag/apriltag.h>
 #include "TagManager.h"
+
 using namespace std;
 
 
 
 int main() {
 	rs2::pipeline pipe;
-
+	Tag_Manager tag = Tag_Manager();
+	pipe.start();
+	cout << "Hello world!";
 	return 0;
 }
